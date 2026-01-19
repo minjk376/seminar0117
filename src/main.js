aae7c53e8723d84c4fe14c6d05ffa743eee53170
minjk376 <158014660+minjk376@users.noreply.github.com>
2026-01-19 15:43:33 +0900

    q

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 const button = document.getElementById("danger-button");
 
 button.addEventListener("click", () => {
-  alert("I Said Do Not Click This Button");
+  Swal.fire({
+    title: "I Said Do Not Click This Button",
+    text: "Why did you do that?",
+    icon: "warning",
+    confirmButtonText: "Sorry 😓",
+    background: "#0f2a4d",
+    color: "#ffffff",
+    confirmButtonColor: "#1b3f7a",
+  });
 });
